92d1387caf7ffc2e3eb35a8c96604ba274738ba6
belal-waheed <belwaheed@gmail.com>
2026-09-05 12:59:16 +0300

    Q2

--- Assignment04/Program.cs
+++ Assignment04/Program.cs
@@ -13,7 +13,20 @@ namespace Assignment04
             Console.WriteLine(prices[1]);
             */
             #endregion
-           
+
+            #region Q2
+            /*
+             Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and
+shelf 1 has 1, 4 copies. Print the number of copies on shelf 1, slot 0.
+             */
+
+            /*
+            int[,] shelfCopies = { { 3, 5 }, { 1, 4 } };
+            Console.WriteLine(shelfCopies[1, 0]);
+             */
+            
+            #endregion
+            
 
         }
     }
