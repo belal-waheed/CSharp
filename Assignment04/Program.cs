d77674a17de7cb8d93ee957687a9b9ce7241bf7f
belal-waheed <belwaheed@gmail.com>
2026-09-05 12:50:10 +0300

    Q1

--- Assignment04/Program.cs
+++ Assignment04/Program.cs
@@ -4,7 +4,17 @@ namespace Assignment04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Assignment 04");
+            #region Q1
+            //Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print
+            //the second price(index 1).
+
+            /*
+            double[] prices = { 25.5, 40.0, 33.75 };
+            Console.WriteLine(prices[1]);
+            */
+            #endregion
+           
+
         }
     }
 }
