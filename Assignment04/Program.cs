37d95e206ca6ca4d173226212ffdc948724182c7
belal-waheed <belwaheed@gmail.com>
2026-09-06 13:25:03 +0300

    Q9&10

--- Assignment04/Program.cs
+++ Assignment04/Program.cs
@@ -125,7 +125,47 @@ afterward.*/
             //double[] prices = { 25.5, 40.0 };
             //ReplaceArray(ref prices);
             //Console.WriteLine(prices.Length); // Output: 3
-            #endregion 
+            #endregion
+
+            #region Q9
+            /*
+           Write a method bool TryGetPrice(string title, out double price) that returns true and sets
+price to 25.5 if title is "Clean Code", otherwise returns false and sets price to 0. Call it
+2
+and print the price if found*/
+            //static bool TryGetPrice(string title, out double price)
+            //{
+            //    if (title == "Clean Code")
+            //    {
+            //        price = 25.5;
+            //        return true;
+            //    }
+
+            //    price = 0;
+            //    return false;
+            //}
+            //if (TryGetPrice("Clean Code", out double price))
+            //{
+            //    Console.WriteLine(price);// Output: 25.5
+            //}
+
+            #endregion
+
+            #region 10
+            /*
+             Write a method PrintBookInfo(string title, int pages = 300) where pages is optional. Call
+it once with only a title, and once passing both a title and pages.
+             */
+            //static void PrintBookInfo(string title, int pages = 300)
+            //{
+            //    Console.WriteLine($"Title: {title}, Pages: {pages}");
+            //}
+            //PrintBookInfo("Clean Code"); // Output: Title: Clean Code, Pages: 300
+
+            //PrintBookInfo("The Pragmatic Programmer", 352); // Output: Title: The Pragmatic Programmer, Pages: 352
+            #endregion
+
+
         }
     }
 }
