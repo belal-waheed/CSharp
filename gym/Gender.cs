9f4dd853e2cfe01093cb60519b2d367b26731f48
belal-waheed <belwaheed@gmail.com>
2026-09-09 12:30:05 +0300

    Gym

---
+++ gym/Gender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gym
+{
+    internal enum Gender
+    {
+        Male,
+        Female
+    }
+}
---
+++ gym/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gym
+{
+    internal class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public MembershipPrice MembershipPrice { get; set; }
+    }
+}
