bdd8f8b508cdc8435503b844d00bd8e2d4cc5205
belal-waheed <belwaheed@gmail.com>
2026-09-13 11:50:47 +0300

    Q1&2

--- OOP01/Program.cs
+++ OOP01/Program.cs
@@ -4,6 +4,48 @@
     {
         static void Main(string[] args)
         {
+            #region 1
+            /*
+             Question 1
+            Consider the following code:
+
+            public struct DeliveryAddress
+            {
+                public string City; public string Street;
+            }
+
+            public class Customer
+            {
+                public string Name;
+            }
+             */
+            //a)  What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
+
+            //When a struct is copied, a new instance is created, and modifying the copy does not affect the original, because its value type.
+
+            //b)  What happens when a Customer variable is copied into another variable and one variable modifies the object?
+
+            // both variables reference the same object in memory.so it will affect the original object because its reference type.
+            #endregion
+
+            #region 2
+            /*
+            Consider the following struct:
+            public struct Shipment
+            {
+                public string Description;
+                public double Weight; 
+                public decimal DeliveryFee;
+            }
+a) a) Identify at least three problems with this design from an encapsulation perspective.
+//No data protection so any code can modify the fields directly.
+//No validation
+//
+b) b) How can private fields and public properties improve this design?
+Private fields hide the internal data, while public properties provide controlled access. 
+Properties can also include validation and other logic before allowing values to be changed.
+             */
+            #endregion
         }
     }
 }
