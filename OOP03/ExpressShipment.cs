ddce7062066bf35c49022ffc00220198190fc0ad
belal-waheed <belwaheed@gmail.com>
2026-09-20 13:30:28 +0300

    derived shipment classes with sealed class and sealed method

---
+++ OOP03/ExpressShipment.cs
@@ -0,0 +1,42 @@
+namespace OOP03
+{
+    public class ExpressShipment : Shipment
+    {
+        private decimal extraFee;
+
+        public decimal ExtraFee
+        {
+            get => extraFee;
+            set
+            {
+                if (value >= 0)
+                    extraFee = value;
+            }
+        }
+
+        public override decimal EstimatedCost => DeliveryFee + (Weight * 5m) + ExtraFee;
+
+        public ExpressShipment(string trackingCode)
+            : base(trackingCode)
+        {
+            ExtraFee = 0m;
+        }
+
+        public ExpressShipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination, decimal extraFee)
+            : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+            ExtraFee = extraFee >= 0 ? extraFee : 0m;
+        }
+
+        public override void PrintShipment()
+        {
+            Console.WriteLine($"Tracking Code: {TrackingCode}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Weight: {Weight} KG");
+            Console.WriteLine($"Delivery Fee: {DeliveryFee} EGP");
+            Console.WriteLine($"Destination: {Destination.GetFullAddress()}");
+            Console.WriteLine($"Extra Fee: {ExtraFee} EGP");
+            Console.WriteLine($"Estimated Cost: {EstimatedCost} EGP");
+        }
+    }
+}
